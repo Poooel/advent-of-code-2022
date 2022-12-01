d6ece24adbb9688b20f887c37cdcb278585fe3f1
Paul Queruel <paul.queruel@protonmail.com>
2022-12-01 23:30:22 +0000

    Refactor Day 01

--- src/main/kotlin/com/github/advent/of/code/days/Day01CalorieCounting.kt
+++ src/main/kotlin/com/github/advent/of/code/days/Day01CalorieCounting.kt
@@ -4,26 +4,29 @@ import com.github.advent.of.code.Executable
 
 class Day01CalorieCounting : Executable {
     override fun executePartOne(input: String): Any {
-        val grossCaloriesByElves = input.split("\n\n")
-
-        val caloriesByElves = grossCaloriesByElves.map { grossCalories ->
-            grossCalories.split("\n").sumOf { it.toInt() }
-        }
+        val grossCaloriesByElves = grossCaloriesByElves(input)
+        val caloriesByElves = caloriesByElves(grossCaloriesByElves)
 
         return caloriesByElves.max()
     }
 
     override fun executePartTwo(input: String): Any {
-        val grossCaloriesByElves = input.split("\n\n")
-
-        val caloriesByElves = grossCaloriesByElves.map { grossCalories ->
-            grossCalories.split("\n").sumOf { it.toInt() }
-        }
+        val grossCaloriesByElves = grossCaloriesByElves(input)
+        val caloriesByElves = caloriesByElves(grossCaloriesByElves)
 
         val sortedCaloriesByElves = caloriesByElves.sorted().reversed()
-
         val (first, second, third) = sortedCaloriesByElves
 
         return first + second + third
+    }
+
+    private fun grossCaloriesByElves(input: String): List<String> {
+        return input.split("\n\n")
+    }
+
+    private fun caloriesByElves(grossCaloriesByElves: List<String>): List<Int> {
+        return grossCaloriesByElves.map { grossCalories ->
+            grossCalories.split("\n").sumOf { it.toInt() }
+        }
     }
 }
